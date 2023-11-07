e948d3df4a9e7d2d4dc2d97f0cb55aaa9b02c93f
erynder-z <stfn.bgr@gmail.com>
2023-11-07 21:58:28 +0100

    Create request to Ollama-API

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -12,20 +12,70 @@ type Model = {
   size: number
 }
 
+type ModelResponse = {
+  model: string
+  created_at: string
+  response: string
+  context: number[]
+  done: boolean
+  total_duration: number
+  load_duration: number
+  sample_count: number
+  sample_duration: number
+  prompt_eval_count: number
+  prompt_eval_duration: number
+  eval_count: number
+  eval_duration: number
+}
+
 export default function Chat({ selectedModel }: ChatProps) {
   const [inputText, setInputText] = useState<string>('')
   const [outputText, setOutputText] = useState<string>('')
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
 
-  const { name } = selectedModel
+  const submitPrompt = async () => {
+    if (selectedModel) {
+      setIsSubmitting(true)
+      try {
+        const apiEndpoint = 'http://localhost:11434/api/generate'
+        const modelName = selectedModel.name
+        const userPrompt = inputText
+        const shouldReturnStream = false
+
+        const response = await fetch(apiEndpoint, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            model: modelName,
+            prompt: userPrompt,
+            stream: shouldReturnStream,
+          }),
+        })
+
+        const modelResponse: ModelResponse = await response.json()
+        setOutputText(modelResponse.response)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setIsSubmitting(false)
+        setInputText('')
+      }
+    }
+  }
 
   return (
     <div className="w-full col-start-2 row-start-1 row-span-3 grid grid-cols-1 grid-rows-3 md:my-40 p-4 leading-7 text-lg bg-stone-300">
       <div className="row-span-2">
-        <header className="text-3xl font-bold">{name}</header>
+        <header className="text-3xl font-bold">{selectedModel.name}</header>
         <OutputField />
       </div>
       <div className="row-span-1 mt-auto">
-        <InputField inputText={inputText} setInputText={setInputText} />
+        <InputField
+          inputText={inputText}
+          setInputText={setInputText}
+          submitPrompt={submitPrompt}
+          isSubmitting={isSubmitting}
+        />
       </div>
     </div>
   )
--- src/components/Chat/InputField/InputField.tsx
+++ src/components/Chat/InputField/InputField.tsx
@@ -1,13 +1,18 @@
 import { useEffect, useRef, useState } from 'react'
+import { TbSend, TbSendOff } from 'react-icons/tb'
 
 type InputFieldProps = {
   inputText: string
   setInputText: React.Dispatch<React.SetStateAction<string>>
+  submitPrompt: () => void
+  isSubmitting: boolean
 }
 
 export default function InputField({
   inputText,
   setInputText,
+  submitPrompt,
+  isSubmitting,
 }: InputFieldProps) {
   const [textareaRows, setTextareaRows] = useState(4)
   const textareaRef = useRef<HTMLTextAreaElement | null>(null)
@@ -25,10 +30,6 @@ export default function InputField({
     setTextareaRows(textarea.rows)
   }
 
-  const handleSubmit = () => {
-    console.log(inputText)
-  }
-
   useEffect(() => {
     if (textareaRef.current) {
       autoResizeTextarea(textareaRef.current)
@@ -37,24 +38,34 @@ export default function InputField({
 
   return (
     <>
-      <div className="relative z-0 flex bg-slate-200 border-l-4 border-indigo-400">
+      <div className="relative z-0 flex bg-slate-200 border-l-4 border-indigo-400 text-slate-900">
         <textarea
           ref={textareaRef}
           rows={textareaRows}
           required
           autoComplete="off"
+          disabled={isSubmitting}
           id="textInput"
           name="textInput"
-          className="block p-2 w-full max-h-40 overflow-y-auto text-sm  bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 peer overflow-hidden resize-none"
+          className={`${
+            isSubmitting
+              ? 'bg-gray-400 text-gray-600'
+              : 'bg-transparent text-slate-900'
+          } block p-2 w-full max-h-40 overflow-y-auto text-sm border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 peer overflow-hidden resize-none`}
           placeholder=" "
           value={inputText}
           onChange={handleTextareaChange}
         />
         <button
-          onClick={handleSubmit}
-          className="p-4 bg-indigo-400 hover:bg-indigo-500"
+          onClick={submitPrompt}
+          disabled={isSubmitting}
+          className="w-20 p-4 flex justify-center items-center bg-indigo-400 hover:bg-indigo-500"
         >
-          Send
+          {isSubmitting ? (
+            <TbSendOff size="1.75em" />
+          ) : (
+            <TbSend size="1.75em" />
+          )}
         </button>
         <label
           htmlFor="textInput"
